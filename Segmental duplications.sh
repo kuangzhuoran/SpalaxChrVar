babcc2d42268e75e9d144c1489a0da257596f2e7
Zhuoran Kuang <76553133+kuangzhuoran@users.noreply.github.com>
2024-04-29 09:49:56 +0800

    Add files via upload

--- Segmental duplications.sh
+++ Segmental duplications.sh
@@ -1,3 +1,5 @@
+#https://github.com/0xTCG/biser
 biser -o golani --gc-heap 1G --threads 40 golani.softmask.fa
+
 cut -f2,3,4 golani.biser.elem.txt | bedtools sort -i | bedtools merge -i - > golani.biser.elem.merge
 awk '{print $3-$2}' golani.biser.elem.merge | awk '{sum += $1};END {print sum}' 
