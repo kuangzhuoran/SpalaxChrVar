79bda068a34f44992a73c7d0010ce609e90bd1d7
Zhuoran Kuang <76553133+kuangzhuoran@users.noreply.github.com>
2024-04-29 09:19:49 +0800

    Add files via upload

--- Identification of centromere repeats.sh
+++ Identification of centromere repeats.sh
@@ -1,2 +1,7 @@
 #https://github.com/zhangrengang/Centromics
 centromics -l hifi.fq.gz -g ref.fa -pre hifi
+
+#dyad symmetries
+#ref:https://doi.org/10.1016/j.cell.2022.06.045
+#use EMBOSS
+./palindrome -sequence EroSat3.fasta -minpallen 4 -maxpallen 100 -gaplimit 20 -nummismatches 0 -overlap -outfile EroSat3.dyad
